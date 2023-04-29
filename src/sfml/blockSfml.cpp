7e8843458ce2489d64a6be22235e6de11c8becf1
HONORE ALEXANDRE p2107949 <alexandre.honore@etu.univ-lyon1.fr>
2023-04-29 15:34:30 +0200

    block display

--- src/sfml/blockSfml.cpp
+++ src/sfml/blockSfml.cpp
@@ -19,10 +19,22 @@ BlockSfml::BlockSfml(Level * level, int spriteSize, int tileX, int tileY)
     else if (path == "0110" && !level->getBlock(tileX-1, tileY+1)->isSolid()) path += "_alt";
     else if (path == "1010" && !level->getBlock(tileX+1, tileY+1)->isSolid()) path += "_alt";
     else if (path == "1001" && !level->getBlock(tileX+1, tileY-1)->isSolid()) path += "_alt";
-    else if (path == "0111" && !level->getBlock(tileX-1, tileY-1)->isSolid() && !level->getBlock(tileX-1, tileY+1)->isSolid()) path += "_alt";
-    else if (path == "1011" && !level->getBlock(tileX+1, tileY-1)->isSolid() && !level->getBlock(tileX+1, tileY+1)->isSolid()) path += "_alt";
-    else if (path == "1110" && tileY-1>=0 && !level->getBlock(tileX, tileY-1)->isSolid()) path += "_alt";
-    else if (path == "1101" && tileY+1<level->getHeight() && !level->getBlock(tileX, tileY+1)->isSolid()) path += "_alt";
+    else if (path == "0111") {
+        if (!level->getBlock(tileX-1, tileY-1)->isSolid() && !level->getBlock(tileX-1, tileY+1)->isSolid()) path += "_alt_b";
+        else if (!level->getBlock(tileX-1, tileY-1)->isSolid()) path += "_alt_u";
+        else if (!level->getBlock(tileX-1, tileY+1)->isSolid()) path += "_alt_d";
+    }
+    else if (path == "1011") {
+        if (!level->getBlock(tileX+1, tileY-1)->isSolid() && !level->getBlock(tileX+1, tileY+1)->isSolid()) path += "_alt_b";
+        else if (!level->getBlock(tileX+1, tileY-1)->isSolid()) path += "_alt_u";
+        else if (!level->getBlock(tileX+1, tileY+1)->isSolid()) path += "_alt_d";
+    }
+    else if (path == "1101" && tileY-1>0) {
+        if (!level->getBlock(tileX-1, tileY-1)->isSolid() && !level->getBlock(tileX+1, tileY-1)->isSolid()) path += "_alt_b";
+        else if (!level->getBlock(tileX-1, tileY-1)->isSolid()) path += "_alt_l";
+        else if (!level->getBlock(tileX+1, tileY-1)->isSolid()) path += "_alt_r";
+    }
+    else if (path == "1110" && tileY+1<level->getHeight() && !level->getBlock(tileX-1, tileY+1)->isSolid() && !level->getBlock(tileX+1, tileY+1)->isSolid()) path += "_alt";
 
     this->texture.loadFromFile("data/blocks/"+path+".png");
     this->sprite.setTexture(this->texture);
